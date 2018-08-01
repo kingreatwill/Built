2cf53f416a2874b2f583d8bc8b3b538c195f1c57
Enter <350840291@qq.com>
2018-08-01 20:59:58 +0800

    添加demo

---
+++ samples/Built.Grpc.ImplSample1/HelloworldImpl.cs
@@ -0,0 +1,17 @@
+﻿using Built.Grpc.ContractsSample1.HelloDemo;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Built.Grpc.ImplSample1
+{
+    public class HelloworldImpl : BuiltHelloDemoSrv.BuiltHelloDemoSrvBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply { Message = "Hello " + request.Name });
+        }
+    }
+}
---
+++ samples/Built.Grpc.SrvSample1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Built.Grpc.ContractsSample1.HelloDemo;
+using Built.Grpc.ImplSample1;
+using Grpc.Core;
+using System;
+
+namespace Built.Grpc.SrvSample1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var server = new Server
+            {
+                Services = { BuiltHelloDemoSrv.BindService(new HelloworldImpl()) },
+                Ports = { new ServerPort("", 0, ServerCredentials.Insecure) }
+            };
+
+            server.Start();
+
+            server.ShutdownAsync().Wait();
+
+            Console.ReadLine();
+        }
+    }
+}
