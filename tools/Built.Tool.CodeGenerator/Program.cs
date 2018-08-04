4478ea30b9071449ffa51fe04c7c0b7f3757d667
Enter <350840291@qq.com>
2018-08-04 13:31:52 +0800

    生成文件

--- tools/Built.Tool.CodeGenerator/Program.cs
+++ tools/Built.Tool.CodeGenerator/Program.cs
@@ -1,11 +1,65 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace Built.Tool.CodeGenerator
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var architecture = RuntimeInformation.OSArchitecture.ToString().ToLower();// 系统架构,x86 x64
+            var os = "windows";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                os = "windows";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                os = "linux";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                os = "macosx";
+            }
+
+            var protoc = $@"packages\google.protobuf.tools\3.5.1\tools\{os}_{architecture}\protoc";//windows  exe
+            var protocPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, protoc);
+            // grpc_csharp_plugin
+            var pluginPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $@"packages\grpc.tools\1.9.0\tools\{os}_{architecture}\grpc_csharp_plugin.exe");
+            var protoc_args = $" --proto_path={Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "protos")} --csharp_out {Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "temp_gen_grpc_code")} helloworld.proto --grpc_out {Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "temp_gen_grpc_code")} --plugin=protoc-gen-grpc={pluginPath}";
+            var psi = new ProcessStartInfo(protocPath, protoc_args)
+            {
+                RedirectStandardOutput = true
+            };
+            //启动
+            var proc = Process.Start(psi);
+            if (proc == null)
+            {
+                Console.WriteLine("Can not exec.");
+            }
+            else
+            {
+                Console.WriteLine("-------------Start read standard output--------------");
+                //开始读取
+                using (var sr = proc.StandardOutput)
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+
+                    if (!proc.HasExited)
+                    {
+                        proc.Kill();
+                    }
+                }
+                Console.WriteLine("---------------Read end------------------");
+                Console.WriteLine($"Total execute time :{(proc.ExitTime - proc.StartTime).TotalMilliseconds} ms");
+                Console.WriteLine($"Exited Code ： {proc.ExitCode}");
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
--- tools/Built.Tool.CodeGenerator/README.md
+++ tools/Built.Tool.CodeGenerator/README.md
@@ -1,5 +1,8 @@
 ﻿根据 proto 生成cs编译成dll
+根据操作系统运行不同的文件
 https://bbs.csdn.net/topics/360024896
 1,csc
 2,roslyn
 3,CSharpCodeProvider
+
+E:\Built\tools\Built.Tool.CodeGenerator\bin\Debug\netcoreapp2.1\packages\google.protobuf.tools\3.5.1\tools\windows_x64\protoc  --csharp_out E:\Built\tools\Built.Tool.CodeGenerator\bin\Debug\netcoreapp2.1\temp_gen_grpc_code --proto_path=E:\Built\tools\Built.Tool.CodeGenerator\bin\Debug\netcoreapp2.1\protos helloworld.proto --grpc_out E:\Built\tools\Built.Tool.CodeGenerator\bin\Debug\netcoreapp2.1\temp_gen_grpc_code --plugin=protoc-gen-grpc=E:\Built\tools\Built.Tool.CodeGenerator\bin\Debug\netcoreapp2.1\packages\grpc.tools\1.9.0\tools\windows_x64\grpc_csharp_plugin.exe
