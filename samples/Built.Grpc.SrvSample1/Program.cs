d1d82c73de179c8e24f400b7fba462a71bfe86a7
LDoreamon <351363421@qq.com>
2018-08-05 16:52:57 +0800

    consul测试

--- samples/Built.Grpc.SrvSample1/Program.cs
+++ samples/Built.Grpc.SrvSample1/Program.cs
@@ -2,6 +2,7 @@
 using Built.Grpc.ContractsSample1.ProductBasic;
 using Built.Grpc.Extensions;
 using Built.Grpc.ImplSample1;
+using Consul;
 using Google.Protobuf.Reflection;
 using Grpc.Core;
 using Grpc.Core.Interceptors;
@@ -35,9 +36,15 @@ namespace Built.Grpc.SrvSample1
             server.StartAndRegisterConsul();
 
             Console.WriteLine("Greeter server listening on port " + Port);
+           
+            Console.ReadKey();
+           var client = new ConsulClient(p =>
+            {
+                p.Address = new Uri("http://127.0.0.1:8500");
+            });
+            var sd = client.Health.Service("HelloDemo.BuiltHelloDemoSrv", "", true).Result;
             Console.WriteLine("Press any key to stop the server...");
             Console.ReadKey();
-
             server.ShutdownAsync().Wait();
 
             Console.ReadLine();
--- src/Built.Grpc/Extensions/GrpcServiceRegisterExtension.cs
+++ src/Built.Grpc/Extensions/GrpcServiceRegisterExtension.cs
@@ -1,9 +1,11 @@
-﻿using Grpc.Core;
+﻿using Consul;
+using Grpc.Core;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using static Grpc.Core.Server;
@@ -17,6 +19,46 @@ namespace Built.Grpc.Extensions
             server.Start();
             var ports = server.Ports;
             var srvNames = server.Services.GetServicesName();
+
+            try
+            {
+                var client = new ConsulClient(p =>
+                {
+                    p.Address = new Uri("http://127.0.0.1:8500");
+                }); 
+                 client.Agent.ServiceRegister(new AgentServiceRegistration {
+                    Address= "127.0.0.1",
+                    Port= ports.First().BoundPort,
+                    Name= srvNames.First(),
+                    Tags= srvNames.ToArray(),
+                    Check=new AgentServiceCheck {
+                        //TTL=TimeSpan.FromSeconds(5),
+                        Status = HealthStatus.Passing,
+                        TCP= "127.0.0.1:50051",
+                        Interval = TimeSpan.FromSeconds(5)
+                    }
+                 }).Wait();
+              
+                //client.Agent.CheckRegister(new AgentCheckRegistration
+                //{
+                //    DeregisterCriticalServiceAfter = TimeSpan.FromMinutes(1),
+                //    TTL = TimeSpan.FromSeconds(ServiceConfig.TCPInterval),
+                //    Status = HealthStatus.Passing,
+                //    ID = ServiceConfig.GetConsulServiceId() + ":ttlcheck",
+                //    ServiceID = ServiceConfig.GetConsulServiceId(),
+                //    Name = "ttlcheck"
+                //}).Wait();
+
+                client.Dispose();
+               
+
+            }
+            catch (Exception ex)
+            {
+                InnerLogger.Log(LoggerLevel.Error, $"consul Register failed {Environment.NewLine}{ex.ToString()}");
+            }
+
+
             return server;
         }
 
@@ -27,6 +69,8 @@ namespace Built.Grpc.Extensions
             {
                 var sd = new ServiceDescriptor(typeof(ServerServiceDefinition), srv);
                 var info = srv.GetType().GetField("callHandlers", BindingFlags.Instance | BindingFlags.NonPublic);
+
+               
                 foreach (var s in (IEnumerable)info.GetValue(srv))
                 {
                     var kv = s.ToString().Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
