6d4bfdc477b108fdde17266c4c17c3b075402a23
Enter <350840291@qq.com>
2018-08-07 20:35:04 +0800

    swagger

--- samples/Built.Grpc.GatewaySwagger.Sample1/Startup.cs
+++ samples/Built.Grpc.GatewaySwagger.Sample1/Startup.cs
@@ -39,7 +39,11 @@ namespace Built.Grpc.GatewaySwagger.Sample1
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
--- src/Built.Grpc.HttpGateway/SwaggerMiddleware.cs
+++ src/Built.Grpc.HttpGateway/SwaggerMiddleware.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Built.Grpc.HttpGateway
 {
-    class SwaggerMiddleware
+    public class SwaggerMiddleware
     {
     }
 }
