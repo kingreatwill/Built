c72329068122ceb13ba6f165964b91cfd936330f
Enter <350840291@qq.com>
2018-08-09 13:38:39 +0800

    EventBus

---
+++ demos/Built.EventBus.MassTransit.ServerDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Built.EventBus.MassTransit.ServerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ demos/Built.EventBus.NServiceBus.ServerDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Built.EventBus.NServiceBus.ServerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
