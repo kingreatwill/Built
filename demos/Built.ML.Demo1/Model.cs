dc057381d8e5b0f824b2dfbb1f603c773488c0d8
Enter <350840291@qq.com>
2018-08-13 11:57:42 +0800

    ML预测双色球

---
+++ demos/Built.ML.Demo1/Model.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.ML.Runtime.Api;
+using System.Text;
+
+namespace Built.ML.Demo1
+{
+    public class TaxiTrip
+    {
+        [Column("0")]
+        public int Num1;
+
+        [Column("1")]
+        public int Num2;
+
+        [Column("2")]
+        public int Num3;
+
+        [Column("3")]
+        public int Num4;
+
+        [Column("4")]
+        public int Num5;
+
+        [Column("5")]
+        public int Num6;
+
+        [Column("6")]
+        public int Red1;
+    }
+
+    public class TaxiTripFarePrediction
+    {
+        [Column("0")]
+        public int Num1;
+
+        [Column("1")]
+        public int Num2;
+
+        [Column("2")]
+        public int Num3;
+
+        [Column("3")]
+        public int Num4;
+
+        [Column("4")]
+        public int Num5;
+
+        [Column("5")]
+        public int Num6;
+
+        [Column("6")]
+        public int Red1;
+    }
+}
---
+++ demos/Built.ML.Demo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Built.ML.Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
