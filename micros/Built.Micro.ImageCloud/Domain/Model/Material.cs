0a55d17d716b18bb2c37d3c1821fe508a9012234
Enter <350840291@qq.com>
2018-08-17 16:36:06 +0800

    0

--- micros/Built.Micro.ImageCloud/Domain/Model/Material.cs
+++ micros/Built.Micro.ImageCloud/Domain/Model/Material.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace Built.Micro.ImageCloud
 {
+    /*
+     voice_count	语音总数量
+video_count	视频总数量
+image_count	图片总数量
+news_count	图文总数量
+media_id
+title	是	标题
+thumb_media_id	是	图文消息的封面图片素材id（必须是永久mediaID）
+author	是	作者
+digest	是	图文消息的摘要，仅有单图文消息才有摘要，多图文此处为空
+show_cover_pic	是	是否显示封面，0为false，即不显示，1为true，即显示
+content	是	图文消息的具体内容，支持HTML标签，必须少于2万字符，小于1M，且此处会去除JS
+content_source_url	是	图文消息的原文地址，即点击“阅读原文”后的URL
+         */
+
     //if you are able to define your model
     public class Material : Entity
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string Name { get; set; }
+        public string FileName { get; set; }
+
+        public MaterialType Type { get; set; }
+
+        public string Author { get; set; }
+
+        public string Content { get; set; }
+    }
+
+    public enum MaterialType
+    {
+        Image = 1,
+        Video = 3,
+        Doc = 5,
     }
 }
