33b079c73e093ca97deee6f119b7081ea2021fd3
Enter <350840291@qq.com>
2018-08-18 12:34:17 +0800

    完善

--- micros/Built.Micro.ImageCloud/Domain/Services/IMaterialService.cs
+++ micros/Built.Micro.ImageCloud/Domain/Services/IMaterialService.cs
@@ -27,11 +27,7 @@ namespace Built.Micro.ImageCloud.Domain.Services
         public async Task<ObjectId> UploadAsync(string filename, byte[] bytes)
         {
             var bucket = new GridFSBucket(Repository.Collection.Database); //这个是初始化gridFs存储的
-            ObjectId fileId = await bucket.UploadFromBytesAsync(filename, bytes, new GridFSUploadOptions
-            {
-                DisableMD5 = false
-            });
-
+            // 计算MD5;
             System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
             var hash = md5.ComputeHash(bytes);
             StringBuilder result = new StringBuilder();
@@ -40,11 +36,25 @@ namespace Built.Micro.ImageCloud.Domain.Services
                 result.Append(hash[i].ToString("x2"));
             }
             var md5Str = result.ToString();
-
-            var fileList = bucket.Find(new { md5 = md5Str }.ToBsonDocument()).ToList();
+            // 处理重复文件;
+            ObjectId fileId;
+            var fileList = bucket.Find(new { md5 = md5Str }.ToBsonDocument(), new GridFSFindOptions
+            {
+                Limit = 1
+            }).ToList();
             if (fileList.Any())
             {
+                var fInfo = fileList.First();
+                fileId = fInfo.Id;
             }
+            else
+            {
+                fileId = await bucket.UploadFromBytesAsync(filename, bytes, new GridFSUploadOptions
+                {
+                    DisableMD5 = false
+                });
+            }
+            // 插入素材记录;
             var entity = new Material
             {
                 FileId = fileId.ToString(),
