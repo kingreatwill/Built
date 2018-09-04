e9585201b9ba67ecdd15d0d6068bcce9953053c3
Enter <350840291@qq.com>
2018-09-04 16:22:02 +0800

    docker.client

---
+++ demos/Built.DockerClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Built.Docker
+{
+    /// <summary>
+    /// 语言版本7.3
+    /// </summary>
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            DockerClient client = new DockerClientConfiguration(new Uri("http://192.168.1.230:4243")).CreateClient();
+
+            IList<ContainerListResponse> containers = await client.Containers.ListContainersAsync(
+            new ContainersListParameters()
+            {
+                Limit = 10,
+            });
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ demos/Built.DockerClient/README.md
@@ -0,0 +1,10 @@
+﻿1、 查看配置文件位于哪里
+systemctl show --property=FragmentPath docker 
+2、编辑配置文件内容，接收所有ip请求
+vim  /lib/systemd/system/docker.service 
+ExecStart=/usr/bin/dockerd -H unix:///var/run/docker.sock -H tcp://0.0.0.0:4243
+3、重新加载配置文件，重启docker daemon
+sudo systemctl daemon-reload 
+sudo systemctl restart docker
+4、测试
+# docker -H localhost:4243 version
