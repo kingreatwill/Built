c4879864f4260fbd6b7baa82c7303bf0e90fb552
Enter <350840291@qq.com>
2018-09-05 16:01:53 +0800

    demo

--- demos/Built.AOP.AspectInjector.Demo1/Program.cs
+++ demos/Built.AOP.AspectInjector.Demo1/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+//using AspectInjector.Broker;
+//using AspectInjector.Samples.Logging.Aspects;
 
 namespace Built.AOP.AspectInjector.Demo1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            //var service = new SampleService();
+            //service.GetCount();
             Console.WriteLine("Hello World!");
         }
     }
+
+    //[Aspect(Aspect.Scope.Global)]
+    //internal class LoggingAspect
+    //{
+    //    [Advice(Advice.Type.Around, Advice.Target.Method)]
+    //    public object HandleMethod([Advice.Argument(Advice.Argument.Source.Name)] string name,
+    //        [Advice.Argument(Advice.Argument.Source.Arguments)] object[] arguments,
+    //        [Advice.Argument(Advice.Argument.Source.Target)] Func<object[], object> method)
+    //    {
+    //        Console.WriteLine($"Executing method {name}");
+
+    //        var sw = Stopwatch.StartNew();
+
+    //        var result = method(arguments);
+
+    //        sw.Stop();
+
+    //        Console.WriteLine($"Executed method {name} in {sw.ElapsedMilliseconds} ms");
+
+    //        return result;
+    //    }
+    //}
+
+    //[Inject(typeof(LoggingAspect))]
+    //public class SampleService
+    //{
+    //    public int GetCount()
+    //    {
+    //        Thread.Sleep(3000);
+
+    //        return 10;
+    //    }
+    //}
 }
