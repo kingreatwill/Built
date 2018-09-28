6621faa17b1a1f86a65cdc49adc54d4a8cfcc157
Enter <350840291@qq.com>
2018-09-28 17:30:58 +0800

    demo

---
+++ demos/Built.SpringNet.Demo01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Built.SpringNet.Demo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
