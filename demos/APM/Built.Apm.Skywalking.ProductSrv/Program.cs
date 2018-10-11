eb31f36b1841fcbd825b4fbce9700373fbd8a7e7
Enter <350840291@qq.com>
2018-10-11 12:32:19 +0800

    skywalking

---
+++ demos/APM/Built.Apm.Skywalking.ProductSrv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Built.Apm.Skywalking.ProductSrv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ demos/APM/Built.Apm.Skywalking.UserSrv/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Built.Apm.Skywalking.UserSrv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
